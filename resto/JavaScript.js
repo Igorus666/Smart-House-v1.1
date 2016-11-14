2b14d3b0a0b018677465b9c84eb7dbad526e14ed
Igorus666 <igor-sn@hotmail.com>
2016-11-13 23:35:06 -0200

    Add files via upload

---
+++ resto/JavaScript.js
@@ -0,0 +1,52 @@
+function lp(){
+    var estado = document.getElementById("estado").innerHTML;
+    if(estado === "1"){
+        document.getElementById("estado").innerHTML="0";
+        document.getElementById("rele").innerHTML="<div class='rele_desligado'></div>";
+        document.getElementById("botao").innerHTML="<a href='/?ligar' class='botao'>Ligar Rele</a>";
+    } else {
+        document.getElementById("estado").innerHTML="1";
+        document.getElementById("rele").innerHTML="<div class='rele_ligado'></div>";
+        document.getElementById("botao").innerHTML="<a href='/?desligar' class='botao'>Desligar Rele</a>";
+    }
+}
+
+function vt(){
+    var estado = document.getElementById("estado").innerHTML;
+    if(estado === "1"){
+        document.getElementById("estado").innerHTML="0";
+        document.getElementById("rele").innerHTML="<div class='rele_desligado'></div>";
+        document.getElementById("botao").innerHTML="<a href='/?ligar' class='botao'>Ligar Display</a>";
+    } else {
+        document.getElementById("estado").innerHTML="1";
+        document.getElementById("rele").innerHTML="<div class='rele_ligado'></div>";
+        document.getElementById("botao").innerHTML="<a href='/?desligar' class='botao'>Desligar Display</a>";
+    }
+}
+
+function bt1(){
+    var estado = document.getElementById("estado").innerHTML;
+    if(estado === "1"){
+        document.getElementById("estado").innerHTML="0";
+        document.getElementById("rele").innerHTML="<div class='rele_desligado'></div>";
+        document.getElementById("botao").innerHTML="<a href='/?ligar' class='botao'>Ligar Botão 1</a>";
+    } else {
+        document.getElementById("estado").innerHTML="1";
+        document.getElementById("rele").innerHTML="<div class='rele_ligado'></div>";
+        document.getElementById("botao").innerHTML="<a href='/?desligar' class='botao'>Desligar Botão 1</a>";
+    }
+}
+
+
+function bt2(){
+    var estado = document.getElementById("estado").innerHTML;
+    if(estado === "1"){
+        document.getElementById("estado").innerHTML="0";
+        document.getElementById("rele").innerHTML="<div class='rele_desligado'></div>";
+        document.getElementById("botao").innerHTML="<a href='/?ligar' class='botao'>Ligar Botão 2</a>";
+    } else {
+        document.getElementById("estado").innerHTML="1";
+        document.getElementById("rele").innerHTML="<div class='rele_ligado'></div>";
+        document.getElementById("botao").innerHTML="<a href='/?desligar' class='botao'>Desligar Botão 2</a>";
+    }
+}
---
+++ sketch_nov10a/sketch_nov10a.ino
@@ -0,0 +1,101 @@
+#include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = {0xF8, 0xA9, 0x63, 0x5C, 0xDF, 0x55}; //physical mac address
+byte ip[] = { 192, 168, 88, 119 }; // ip in lan
+byte gateway[] = { 192, 168, 88, 1 }; // internet access via router
+byte subnet[] = { 255, 255, 255, 0 }; //subnet mask
+EthernetServer server(80); //server port
+
+String readString;
+
+int pin = 9;
+boolean ligado = true;
+
+//////////////////////
+
+void setup() {
+
+  pinMode(pin, OUTPUT); //pin selected to control
+  //start Ethernet
+  Ethernet.begin(mac, ip, gateway, subnet);
+  server.begin();
+  //the pin for the servo co
+  //enable serial data print
+  Serial.begin(9600);
+  Serial.println('RoboCore Remote Automation V1.1'); // so I can keep track of what is loaded
+}
+
+void loop() {
+  // Create a client connection
+  EthernetClient client = server.available();
+  if (client) {
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+
+        //read char by char HTTP request
+        if (readString.length() < 100) {
+
+          //store characters to string
+          readString += c;
+          //Serial.print(c);
+        }
+
+        //if HTTP request has ended
+        if (c == '\n') {
+
+          ///////////////////// control arduino pin
+          Serial.println(readString); //print to serial monitor for debuging
+          if (readString.indexOf('?ligar') > 0) //checks for on
+          {
+            digitalWrite(pin, HIGH);    // set pin 4 high
+            Serial.println('On');
+            ligado = false;
+          }
+          else {
+            if (readString.indexOf('?desligar') > 0) //checks for off
+            {
+              digitalWrite(pin, LOW);    // set pin 4 low
+              Serial.println('Off');
+              ligado = true;
+            }
+          }
+          //clearing string for next read
+          readString = '';
+
+
+          ///////////////
+
+          client.println("HTTP/1.1 200 OK"); //send new page
+          client.println("Content-Type: text/html");
+          client.println();
+          client.println("<html>");
+          client.println("<head>");
+          client.println("<meta charset='utf-8'>");
+          client.println("<meta http-equiv='X-UA-Compatible' content='IE=edge'>");
+          client.println("<title>Ardulindo</title>");
+          client.println("<link rel='stylesheet' href='resto/CSS.css'>");
+          client.println("<script type='text/javascript' src='resto/JavaScript.js'></script>");
+          client.println("</head>");
+          client.println("<body>");
+          client.println("<button type='button' class='la' onclick='lp()'></button>");
+          client.println("<button type='button' class='Vent' onclick='vt()'></button>");
+          client.println("<button type='button' class='E1' onclick='bt1()'></button>");
+          client.println("<button type='button' class='E2' onclick='bt2()'</button>");
+          client.println("</body>");
+          client.println("</html>");
+
+          delay(1);
+          //stopping client
+          client.stop();
+
+
+
+        }
+      }
+    }
+  }
+}
+
+
